d23c9217bd2e803c4f63c4500a581b8453b5c898
Your Name <you@example.com>
2024-05-10 18:26:33 +0800

    ...

--- runme.sh
+++ runme.sh
@@ -6,9 +6,9 @@ do_cmd(){
 
 . ~/jvenv311/bin/activate
 ./shutdown_all.sh
-for port in 20000 20001 20002 7861 8501; do
-    do_cmd fuser -k $port/tcp
-done
+#for port in 20000 20001 20002 7861 8501; do
+#    do_cmd fuser -k $port/tcp
+#done
 python -m pip install "pydantic>=1.10.11,<2.0"
 python startup.py -a
 
